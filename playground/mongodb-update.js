8f15b97a54c9b92dfa7e6afc79cdd8df8decacb7
aprtm <gharaszti@tekmexico.com>
2019-04-23 17:38:11 -0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,25 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5cbf5345cfbb685884962788')
+    }, { 
+        $set: {
+            name: 'Andrew'
+            },
+        $inc: { age: 1
+        }
+        }, {
+            returnOriginal: false
+        }).then((result) => {
+            console.log(result);
+        });
+
+    //client.close();
+});

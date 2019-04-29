c42a7e97e422ac2ba79be167c91caa4b4c618128
aprtm <gharaszti@tekmexico.com>
2019-04-29 11:39:22 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //Modules
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //Local scripts
 var {mongoose} = require('./db/mongoose');
@@ -64,6 +65,25 @@ app.get('/todos', (req, res) => {
     }, (e) => {
       res.status(400).send(e);
     });
+});
+
+//GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+          return res.status(404).send();
+    } 
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+              return res.status(404).send();
+        }
+
+        res.send({todo});     
+     }).catch((e) => {
+        res.status(400).send();
+     });
 });
 
 app.listen(3000, () => {
